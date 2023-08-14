69441f4a267cb48f114087592b6e161c5669e86c
nexusraman <ramandeep.9333@gmail.com>
2023-08-14 14:50:46 -0400

    Formatting test file

--- backend/test/primeMedianController.test.js
+++ backend/test/primeMedianController.test.js
@@ -1,30 +1,34 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-import request from 'supertest';
-import { app } from '../index'; // Assuming your Express app is exported as `app`
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+import request from 'supertest'
+import { app } from '../index'
 
 describe('Prime Median Controller', () => {
   it('should return medians for a valid positive number greater than 2', async () => {
-    const response = await request(app).get('/api/primeMedian?n=10');
-    expect(response.status).to.equal(200);
-    expect(response.body.medians).to.deep.equal([3, 5]);
-  });
+    const response = await request(app).get('/api/primeMedian?n=10')
+    expect(response.status).to.equal(200)
+    expect(response.body.medians).to.deep.equal([3, 5])
+  })
 
   it('should return an error for negative input', async () => {
-    const response = await request(app).get('/api/primeMedian?n=-5');
-    expect(response.status).to.equal(400);
-    expect(response.body.error).to.equal('Invalid input. Please Enter a positive number');
-  });
+    const response = await request(app).get('/api/primeMedian?n=-5')
+    expect(response.status).to.equal(400)
+    expect(response.body.error).to.equal(
+      'Invalid input. Please Enter a positive number'
+    )
+  })
 
   it('should return an error for input less than 3', async () => {
-    const response = await request(app).get('/api/primeMedian?n=1');
-    expect(response.status).to.equal(400);
-    expect(response.body.error).to.equal('Invalid input. Please Enter a number greater than 2');
-  });
+    const response = await request(app).get('/api/primeMedian?n=1')
+    expect(response.status).to.equal(400)
+    expect(response.body.error).to.equal(
+      'Invalid input. Please Enter a number greater than 2'
+    )
+  })
 
   it('should return an error for non-numeric input', async () => {
-    const response = await request(app).get('/api/primeMedian?n=abc');
-    expect(response.status).to.equal(400);
-    expect(response.body.error).to.equal('Invalid input. Please Enter a number');
-  });
-});
+    const response = await request(app).get('/api/primeMedian?n=abc')
+    expect(response.status).to.equal(400)
+    expect(response.body.error).to.equal('Invalid input. Please Enter a number')
+  })
+})
